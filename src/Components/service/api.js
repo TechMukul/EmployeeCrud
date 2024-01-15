71df79a45f7fb095781f861c2e3d02c077bb786b
TechMukul <133253293+TechMukul@users.noreply.github.com>
2024-01-15 08:26:47 +0530

    changes

--- src/Components/service/api.js
+++ src/Components/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/users";
+const API_URL = "http://localhost:8080";
 export const Adduser = async (data) => {
   try {
     return await axios.post(API_URL, data);
